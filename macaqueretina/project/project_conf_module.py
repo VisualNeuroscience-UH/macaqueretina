VisualNeuroscience-UH/macaqueretina

commit 04ddd786f0ec0543fb700c5c9c33252e3610e3ca

core_parameters.yaml and project_conf_module.py, WIP: added a dispatcher to handle different user requests for each subprocess

macaqueretina/project/project_conf_module.py
@@ -21,7 +21,7 @@ project_conf_module_file_path = Path(__file__).resolve()
 git_repo_root_path = project_conf_module_file_path.parent.parent
 
 
-def _load_parameters() -> ConfigManager:
+def _load_parameters() -> "ConfigManager":
     from macaqueretina.config.config_manager import load_yaml
 
     retina_yaml: str = os.getcwd() + "/macaqueretina/config/retina_parameters.yaml"
@@ -43,6 +43,16 @@ def _load_parameters() -> ConfigManager:
     return config
 
 
+def dispatcher(PM: "ProjectManager", config: "ConfigManager"):
+    breakpoint()
+    if config.run.build_retina:
+        PM.construct_retina.build_retina_client()
+    if config.run.make_stimulus:
+        PM.stimulate.make_stimulus_video()
+    if config.run.simulate_retina:
+        PM.simulate_retina.client()
+
+
 def main():
     config = _load_parameters()
 
@@ -57,10 +67,9 @@ def main():
 
     PM = ProjectManager(config)
 
-    PM.construct_retina.build_retina_client()
+    dispatcher(PM, config)
+
     PM.viz.show_DoG_img_grid(gc_list=[0, 1, 5, 10], savefigname=None)
-    PM.stimulate.make_stimulus_video()
-    PM.simulate_retina.client()
     PM.viz.show_all_gc_responses(savefigname=None)
 
     end_time = time.time()

macaqueretina/config/core_parameters.yaml
@@ -8,3 +8,9 @@ testrun: False  # Stops execution before heavy calculations
 numpy_seed: 42  # random.randint(0, 1000000)  # 42
 device: "cpu"  # "cpu" or "cuda"
 profile: False
+
+run:
+  build_retina: True
+  make_stimulus: True
+  simulate_retina: True
+  visualize: null

commit 3d8369d722b44d373b2c27b14bca8b2db177eaf8

Removed breakpoint introduced in 04ddd78

macaqueretina/project/project_conf_module.py
@@ -44,7 +44,6 @@ def _load_parameters() -> "ConfigManager":
 
 
 def dispatcher(PM: "ProjectManager", config: "ConfigManager"):
-    breakpoint()
     if config.run.build_retina:
         PM.construct_retina.build_retina_client()
     if config.run.make_stimulus: