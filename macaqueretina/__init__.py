ce4b2a6803b4cc157965d6038c7dc8ed50df5c8c
francescangelo <francescangelo.vedele@helsinki.fi>
2025-09-04 14:39:22 +0300

    __init__.py, minimal code for packaging (import macaqueretina in other projects)

--- macaqueretina/__init__.py
+++ macaqueretina/__init__.py
@@ -1,3 +1,20 @@
+# Local
+from .project.project_conf_module import load_parameters as _lp
+from .project.project_manager_module import ProjectManager as _PM
+
+config = _lp()
+
+PM = _PM(config)
+
+construct_retina = PM.construct_retina
+stimulate = PM.stimulate
+simulate_retina = PM.simulate_retina
+viz = PM.viz
+
+__all__ = ["construct_retina", "stimulate", "simulate_retina", "viz"]
+
+del _lp, _PM, PM
+
 # import os
 # import tomli
 
