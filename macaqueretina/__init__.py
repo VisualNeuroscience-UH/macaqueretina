3095399486957bb4822d98a1a07ec8ae59f0c9d1
francescangelo <francescangelo.vedele@helsinki.fi>
2025-09-11 16:59:50 +0300

    __init__.py, exposed retina_math

--- macaqueretina/__init__.py
+++ macaqueretina/__init__.py
@@ -5,17 +5,19 @@ Macaque retina simulator.
 # Local
 from .project.project_conf_module import load_parameters as _lp
 from .project.project_manager_module import ProjectManager as _PM
+from .retina.retina_math_module import RetinaMath
 
 config = _lp()
 
-PM = _PM(config)
+PM: _PM = _PM(config)
 
 construct_retina = PM.construct_retina
 stimulate = PM.stimulate
 simulate_retina = PM.simulate_retina
 viz = PM.viz
+retina_math: RetinaMath = PM.retina_math
 
-__all__ = ["construct_retina", "stimulate", "simulate_retina", "viz"]
+__all__ = ["construct_retina", "stimulate", "simulate_retina", "viz", "retina_math"]
 
 del (_lp, _PM)
 
