d7c2214198bd80c337e77c074a793b0a7fcfa0fd
francescangelo <francescangelo.vedele@helsinki.fi>
2025-09-11 16:58:35 +0300

    __init__.pyi, created file to handle import interface

---
+++ macaqueretina/__init__.pyi
@@ -0,0 +1,14 @@
+"""Type stubs for macaqueretina package."""
+
+# Local
+from .project.project_manager_module import ProjectManager
+from .retina.retina_math_module import RetinaMath
+
+PM: ProjectManager
+# construct_retina:
+# stimulate:
+# simulate_retina:
+# viz:
+retina_math: RetinaMath
+
+__all__: list[str]
