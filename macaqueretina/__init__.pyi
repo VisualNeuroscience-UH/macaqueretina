de9033b3f1f22daf48f15046b867294c62e06177
francescangelo <francescangelo.vedele@helsinki.fi>
2025-09-12 13:31:59 +0300

    __init__.pyi, added better type stubs (WIP)

--- macaqueretina/__init__.pyi
+++ macaqueretina/__init__.pyi
@@ -1,14 +1,74 @@
-"""Type stubs for macaqueretina package."""
+"""Type stubs for MacaqueRetina package."""
+
+# Built-in
+from typing import Any
 
 # Local
+from .data_io.config_io import ConfigManager
 from .project.project_manager_module import ProjectManager
 from .retina.retina_math_module import RetinaMath
 
+config: ConfigManager
+
 PM: ProjectManager
-# construct_retina:
-# stimulate:
-# simulate_retina:
-# viz:
+
+def build_retina() -> None:
+    """Build the retina.
+
+    For each call, a new retina is built with the parameters in the mr.config object.
+
+    After being built, the retina is saved to mr.config.output_folder.
+    """
+    ...
+
+def make_stimulus(options: Any) -> None:
+    """
+    Valid stimulus_options include
+
+    image_width: in pixels
+    image_height: in pixels
+    container: file format to export
+    codec: compression format
+    fps: frames per second
+    duration_seconds: stimulus duration
+    baseline_start_seconds: midgray at the beginning
+    baseline_end_seconds: midgray at the end
+    pattern:
+        'sine_grating'; 'square_grating'; 'colored_temporal_noise'; 'white_gaussian_noise';
+        'natural_images'; 'natural_video'; 'temporal_sine_pattern'; 'temporal_square_pattern';
+        'spatially_uniform_binary_noise'
+    stimulus_form: 'circular'; 'rectangular'; 'annulus'
+    stimulus_position: in degrees, (0,0) is the center.
+    stimulus_size: In degrees. Radius for circle and annulus, half-width for rectangle.
+    contrast: between 0 and 1
+    mean: mean stimulus intensity between 0, 256
+
+    Note if mean + ((contrast * max(intensity)) / 2) exceed 255 or if
+            mean - ((contrast * max(intensity)) / 2) go below 0
+            the stimulus generation fails
+
+    For sine_grating and square_grating, additional arguments are:
+    spatial_frequency: in cycles per degree
+    temporal_frequency: in Hz
+    orientation: in degrees
+
+    For all temporal and spatial gratings, additional argument is
+    phase_shift: between 0 and 2pi
+
+    For spatially_uniform_binary_noise, additional argument is
+    on_proportion: between 0 and 1, proportion of on-stimulus, default 0.5
+    direction: 'increment' or 'decrement'
+    stimulus_video_name: name of the stimulus video
+
+    ------------------------
+    Output: saves the stimulus video file to output path if stimulus_video_name is not empty str or None
+    """
+    ...
+
+def simulate_retina():
+    """Simulate the retina."""
+    ...
+
 retina_math: RetinaMath
 
 __all__: list[str]
