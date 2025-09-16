VisualNeuroscience-UH/macaqueretina

commit 0f11cdf605d48d13a57f1ea710ca1fa7d950d65f

project_conf_module, linting after ff6d5552

macaqueretina/project/project_manager_module.py
@@ -135,7 +135,7 @@ class ProjectManager(ProjectUtilities):
 
         fit = Fit(project_data, self.context.dog_metadata_parameters)
 
-        retina_vae = RetinaVAE(self.context)
+        retina_vae = RetinaVAE(self.context.as_dict())
 
         construct_retina = ConstructRetina(
             self.context,

macaqueretina/project/project_conf_module.py
@@ -31,7 +31,8 @@ def _validation_switch(base: Path) -> Callable | None:
     match len(validation_files):
         case 0:
             print(
-                f"No validation file provided in {base}. Proceeding without parameter validation."
+                f"No validation file provided in {base}. "
+                f"Proceeding without parameter validation."
             )
             return None
         case 1:
@@ -60,7 +61,7 @@ def load_parameters() -> "ConfigManager":
     base: Path = git_repo_root_path.joinpath("parameters/")
     yaml_files = list(base.glob("*.yaml"))
 
-    validate_params: Callable = _validation_switch(base)
+    validate_params: Callable | None = _validation_switch(base)
 
     config: ConfigManager = load_yaml(yaml_files)
 

commit 2e64ba5255d9b7c83a3cf9def33d17d8e1c8f049

ProjectManager, removed wrong passing of self.context to RetinaVAE (accidentally introduced in 0f11cdf6)

macaqueretina/project/project_manager_module.py
@@ -135,7 +135,7 @@ class ProjectManager(ProjectUtilities):
 
         fit = Fit(project_data, self.context.dog_metadata_parameters)
 
-        retina_vae = RetinaVAE(self.context.as_dict())
+        retina_vae = RetinaVAE(self.context)
 
         construct_retina = ConstructRetina(
             self.context,