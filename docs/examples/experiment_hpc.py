c6f93815d96cbf0d3100a8a8d0c50a9edf7ac729
simo <simo.vanni@helsinki.fi>
2026-02-03 18:04:01 +0200

    experiment_hpc for valid_fr4c

--- docs/examples/experiment_hpc.py
+++ docs/examples/experiment_hpc.py
@@ -38,7 +38,7 @@ mr.config.experiment_parameters = {
     "distributions": {"uniform": None},
 }
 
-filename = mr.experiment.build_and_run(build_without_run=True)
+filename = mr.experiment.build_and_run(build_without_run=False)
 
 ########################################
 ## Analyze and visualize experiment ###
