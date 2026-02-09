2ced526411d503b50a4afa8777e7864f8f5cb3b2
Francescangelo <francescangelo.vedele@helsinki.fi>
2026-02-09 11:57:16 +0200

    example_gc_types.py, changed to reflect the new exposed methods

--- docs/examples/example_gc_types.py
+++ docs/examples/example_gc_types.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 # Local
 import macaqueretina as mr
 
+mr.load_parameters()
 mr.make_stimulus()
 
 gc_types = ["parasol", "midget"]
@@ -13,7 +14,7 @@ for gc_type in gc_types:
     for response_type in response_types:
         mr.config.retina_parameters.gc_type = gc_type
         mr.config.retina_parameters.response_type = response_type
-        mr.construct_retina()
+        mr.build_retina()
         mr.simulate_retina()
         mr.viz.show_all_gc_responses(savefigname=None)
 
