6e9f053fc2d28ccd92e24f9603fe2852991670c0
simo <simo.vanni@helsinki.fi>
2026-07-21 10:05:08 +0300

    example_image_reconstruction, combined with roihu version, new display functioning

--- docs/examples/example_image_reconstruction.py
+++ docs/examples/example_image_reconstruction.py
@@ -1,8 +1,10 @@
+import os
 import shutil
 import time
 from pathlib import Path
 
 # Third-party
+import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
@@ -34,18 +36,40 @@ dataset = "test", 834 images available
 3) operation = "simulate" # Creates the retina spike data for testing the model
 4) operation = "reconstruct" # Reconstructs the images from the test spike data
 """
+# # Fluid parameters.
+# dataset = os.environ["DATASET"]  # "train" or "test"
+# n_images = int(os.environ["N_IMAGES"])
+# operation = os.environ["OPERATION"]  # "transform_images", "simulate", "construct_model", or "reconstruct"
+# spatial_model_type = os.environ["SPATIAL_MODEL_TYPE"]  # "DOG" or "VAE"
+# temporal_model_type = os.environ[
+#     "TEMPORAL_MODEL_TYPE"
+# ]  # "fixed", "dynamic" or "subunit"
+# array_idx = int(os.environ["ARRAY_IDX"])
+# array_idx_str = f"{array_idx:02d}"
+# H = W = int(os.environ["HEIGHT_AND_WIDTH"])
 
 # Fluid parameters.
 dataset = "train"  # "train" or "test"
 n_images = 10
-operation = "display"  # "simulate", "construct_model", "reconstruct", "display"
+operation = "simulate"  # "transform_images", "simulate", "construct_model", "reconstruct", "display"
 spatial_model_type = "DOG"  # "DOG" or "VAE"
 temporal_model_type = "fixed"  # "fixed", "dynamic" or "subunit"
-mr.config.experiment = "image_reconstruction_hpc"
-
-image_rootpath = Path(f"/opt3/images/vanHateren/imc_images_{dataset}")
+array_idx_str = "00"
 H = W = 120
+
+mr.config.experiment = "image_reconstruction_hpc_240_tmp"
+image_rootpath = Path(f"/opt3/images/vanHateren/imc_images_{dataset}")
 # torch.manual_seed(42)
+
+# QA Print captured env vars
+print("The following ENV VAR captured")
+print(f"{dataset=}")
+print(f"{n_images=}")
+print(f"{operation=}")
+print(f"{spatial_model_type=}")
+print(f"{temporal_model_type=}")
+print(f"{array_idx_str=}")
+print(f"{H=}, {W=}")
 
 if dataset == "test" and operation == "construct_model":
     raise ValueError("Do not construct model with test dataset!")
@@ -61,10 +85,10 @@ mr.config.path = Path(mr.config.model_root_path).joinpath(
 mr.config.retina_parameters.spatial_model_type = spatial_model_type
 mr.config.retina_parameters.temporal_model_type = temporal_model_type
 
-session_suffix = f"{H}x{W}_{spatial_model_type}_{temporal_model_type}"
+session_suffix = f"{H}x{W}_{spatial_model_type}_{temporal_model_type}_{array_idx_str}"
 
 mr.config.output_folder = mr.config.path / f"resolution_{session_suffix}"
-mr.config.stimulus_folder = mr.config.path / f"stim_resolution_{session_suffix}"
+mr.config.stimulus_folder = mr.config.path / f"stim_resolution_{H}_{W}_{array_idx_str}"
 mr.config.input_folder = None
 model_filename = f"W_{session_suffix}.npz"
 
@@ -381,11 +405,11 @@ mr.config.visual_stimulus_parameters.pattern = "natural_image"
 gc_types = ["parasol", "midget"]
 response_types = ["on", "off"]
 
+output_dir = mr.config.path / f"transformed_{dataset}_images"
+
 
 def simulate_retina():
     # Save transformed images to disk for simulation
-    output_dir = mr.config.path / f"_transformed_{session_suffix}"
-    transformed_filenames = save_transformed_images(output_dir, data_loader)
 
     for gc_type in gc_types:
         for response_type in response_types:
@@ -393,6 +417,12 @@ def simulate_retina():
             mr.config.retina_parameters.response_type = response_type
 
             mr.retina_constructor.construct()  # Reuses existing matching retina
+            transformed_filenames = output_dir.glob("*.png")
+            # If empty, raise an error
+            if not any(transformed_filenames):
+                raise FileNotFoundError(
+                    f"No transformed images found in {output_dir}. Please run 'transform_images' first."
+                )
 
             for this_name in transformed_filenames:
                 mr.config.external_stimulus_parameters.ext_stimulus_file = str(
@@ -412,10 +442,25 @@ def simulate_retina():
                 mr.retina_simulator.simulate(filename=simulation_results_filename)
 
     # Remove the transformed images after simulation
-    shutil.rmtree(output_dir, ignore_errors=True)
+    # shutil.rmtree(output_dir, ignore_errors=True)
+
+
+reco = ImageReconstruction(mr.config, mr.data_io)
 
 
 match operation:
+    case "transform_images":
+        if output_dir.is_dir():
+            raise FileExistsError(
+                f"""
+                Output directory {output_dir} already exists.
+                Currently, we want the same images with different model combinations, 
+                separately for train and test datasets."""
+            )
+        data_loader = get_vanhateren_dataloader(
+            batch_size=32, shuffle=True, num_workers=4
+        )
+        transformed_filenames = save_transformed_images(output_dir, data_loader)
     case "simulate":
         simulate_retina()
 
@@ -439,8 +484,6 @@ match operation:
         )
 
     case "reconstruct":
-        reco = ImageReconstruction(mr.config, mr.data_io)
-
         if Path(mr.config.path / model_filename).is_file():
             model_data = mr.data_io.load_data(filename=model_filename)
             W = model_data["W"]
@@ -453,8 +496,6 @@ match operation:
         R_test, S_test, retina_mask = reco.get_spikes_and_images(
             n_images=n_images, gc_types=gc_types, response_types=response_types
         )
-
-        S_test_img = reco.reconstruct_images(S_test, retina_mask)
 
         S_estimated = reco.estimate_model(W, R_test, S_mean)
         S_estimated_img = reco.reconstruct_images(S_estimated, retina_mask)
@@ -476,6 +517,7 @@ match operation:
     case "display":
         spatial_models = ["DOG", "VAE"]
         temporal_models = ["fixed", "dynamic", "subunit"]
+        stimulus_sample = range(2)
 
         # Create all possible session suffixes based on the model combinations
         model_combinations = [
@@ -484,6 +526,11 @@ match operation:
             for spatial_model in spatial_models
             for temporal_model in temporal_models
         ]
+        n_cond = len(model_combinations)
+
+        #############################
+        # Load the correlation values
+        #############################
 
         # Get all reconstruction result files matching the session suffix
         reconstruction_files = {}
@@ -500,11 +547,55 @@ match operation:
                 len(reconstruction_files.keys()),
             )
         )
+
         # Get the correlation values for each reconstruction file
         for i, session_suffix in enumerate(model_combinations):
             for j, file in enumerate(reconstruction_files[session_suffix]):
                 data = mr.data_io.load_data(filename=file, hush=True)
                 rho_values[j, i] = data["rho"]
+
+        ###########################################################
+        # Init S_test and S_estimated arrays with the correct shape
+        ###########################################################
+
+        # TÄHÄN JÄIT:
+        # 0) VÄHENNÄ RETINAN GC PAIKKOJEN KOHINAA ad 0
+
+        # Read one file to get the shape of S_test and S_estimated
+        sample_file = reconstruction_files[model_combinations[0]][0]
+        sample_data = mr.data_io.load_data(filename=sample_file, hush=True)
+
+        S_test = sample_data["S_test"][stimulus_sample]
+        S_test_img = np.zeros((*S_test.shape[:1], H, W))
+
+        S_estimated = sample_data["S_estimated"][stimulus_sample]
+        S_estimated_img = np.zeros((*S_estimated.shape[:1], n_cond, H, W))
+        retina_mask = sample_data["retina_mask"]
+        # for i in range(2):
+
+        for this_sample_idx in stimulus_sample:
+            S_test_img[this_sample_idx, ...] = reco.reconstruct_images(
+                sample_data["S_test"][stimulus_sample[this_sample_idx]], retina_mask
+            )
+        for i in range(n_cond):
+            session_suffix = model_combinations[i]
+            try:
+                file = reconstruction_files[session_suffix][0]
+                data = mr.data_io.load_data(filename=file, hush=True)
+
+                # breakpoint()
+                S_estimated_img[:, i, ...] = reco.reconstruct_images(
+                    data["S_estimated"][stimulus_sample], retina_mask
+                )
+
+            except IndexError:
+                print(
+                    f"No reconstruction file found for session_suffix: {session_suffix}"
+                )
+
+        ##########################################
+        # Analyze and group the correlation values
+        ##########################################
 
         # returns array of shape (2, 6): [lower, upper] for each column
         cis, bootstrap_samples = bootstrap_ci(rho_values)
@@ -520,36 +611,86 @@ match operation:
         subunit = rho_values[:, [2, 5]].flatten()
         temporal_stat, temporal_p = kruskal(fixed, dynamic, subunit)
 
+        ####################
+        # Create the figure
+        ####################
+        fig = plt.figure()
+        outer = gridspec.GridSpec(
+            2, 2, height_ratios=[1, 2], width_ratios=[1, 6], wspace=0.1, hspace=0.1
+        )
+
+        # Top-level panels
+        ax01 = fig.add_subplot(outer[0, 1])
+        ax10 = fig.add_subplot(outer[1, 0])
+        ax11 = fig.add_subplot(outer[1, 1])
+
+        # Nested 2x6 subgrid in [1]
+        test_inner = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=outer[1, 0])
+        reco_inner = gridspec.GridSpecFromSubplotSpec(2, 6, subplot_spec=outer[1, 1])
+        # axes = [[i, j] for i in range(6) for j in range(2)]
+        test_axes = [fig.add_subplot(test_inner[i]) for i in stimulus_sample]
+        reco_axes = [
+            fig.add_subplot(reco_inner[i, j])
+            for i in stimulus_sample
+            for j in range(n_cond)
+        ]
+
         # Make a bar graph of the mean of the correlation values for each model combination. Add SEM error bars to the bar graph.
-        fig, ax = plt.subplots(2, 1, figsize=(10, 5))
         mean_rho_values = rho_values.mean(axis=0)
-        ax[0].bar(
+        ax01.bar(
             model_combinations,
             mean_rho_values,
             yerr=[mean_rho_values - cis[0, :], cis[1, :] - mean_rho_values],
             capsize=20,
             linewidth=1,
         )
-        ax[0].set_ylim(0.6, 1)
-        ax[0].set_xlabel("Model Combination")
-        ax[0].set_ylabel("Mean Correlation (rho)")
-        ax[0].set_title("Mean Correlation between Reconstructed and Original Images")
+        ax01.set_ylim(0.3, 1)
+        ax01.set_xlabel("Model Combination")
+        ax01.set_ylabel("Mean Correlation (rho)")
+        ax01.set_title("Mean Correlation between Reconstructed and Original Images")
 
         # Annotate the figure with the p-values and statistics from the statistical tests
-        ax[0].text(
+        ax01.text(
             0.5,
             0.95,
             f"Spatial: U = {spatial_stat:.4f}, p = {spatial_p:.4f}, test: Mann-Whitney U",
-            transform=ax[0].transAxes,
+            transform=ax01.transAxes,
         )
-        ax[0].text(
+        ax01.text(
             0.5,
             0.90,
             f"Temporal: H = {temporal_stat:.4f}, p = {temporal_p:.4f}, test: Kruskal-Wallis",
-            transform=ax[0].transAxes,
+            transform=ax01.transAxes,
         )
 
-        # TÄHÄN JÄIT: DEMOA REKONSTRUKTIOt
+        # Show test images.
+
+        for i in stimulus_sample:
+            test_axes[i].imshow(
+                S_test_img[i, ...],
+                cmap="gray",
+                vmin=0,
+                vmax=255,
+            )
+
+        # Show sample images.
+        for i in range(len(stimulus_sample)):
+            for j in range(n_cond):
+                session_suffix = model_combinations[j]
+                ax = reco_axes[i * n_cond + j]
+
+                ax.imshow(
+                    S_estimated_img[i, j, ...],
+                    cmap="gray",
+                    vmin=0,
+                    vmax=255,
+                )
+                ax.set_title(session_suffix)
+                ax.axis("off")
+
+        # ax01.axis("off")
+        ax10.axis("off")
+        ax11.axis("off")
 
         plt.tight_layout()
 
